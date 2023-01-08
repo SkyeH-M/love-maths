5fcc047177e11a2c85cada757d6f688c6195cda0
SkyeH-M <justskye98@gmail.com>
2023-01-08 10:41:04 +0000

    Finished division code, all working

--- assets/js/script.js
+++ assets/js/script.js
@@ -106,8 +106,7 @@ function calculateCorrectAnswer() {
     } else if (operator === '-') {
         return [operand1 - operand2, "subtract"];
     } else if (operator === '/') {
-        let divOperand1 = operand1 * operand2;
-        return [divOperand1 / operand1 || operand2, "division"];
+        return [operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -148,7 +147,7 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivisionQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operand1').textContent = (operand1 * operand2);
+    document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "/";
 }
